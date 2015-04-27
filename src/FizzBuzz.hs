2fc6c2ecbdf0a5c45beb85ee756fa5773825f2d6
Steve McCarthy <steve@redlua.com>
2015-04-27 00:46:50 -0400

    Generalize fizz buzz options, version bump

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -7,13 +7,27 @@ where
 
 import Prelude hiding (unlines)
 import Data.Text (Text, pack, unlines)
+import Data.Monoid ((<>))
 
 fizzBuzzer :: (Int, Int) -> Text
 fizzBuzzer (x, y) = unlines $ map fizzer [x..y]
 
 fizzer :: Int -> Text
-fizzer x
-  | mod x 3 == 0 && mod x 5 == 0 = "FizzBuzz"
-  | mod x 3 == 0                 = "Fizz"
-  | mod x 5 == 0                 = "Buzz"
-  | otherwise                    = pack $ show x
+fizzer = tupleAdjust . vFizzer [(3, "Fizz"), (5, "Buzz")]
+
+type Soda = (Int, Text)
+
+tupleAdjust :: (Int, Text) -> Text
+tupleAdjust (x,"") = pack $ show x
+tupleAdjust (_, t) = t
+
+vFizzer :: [Soda] -> Int -> (Int, Text)
+vFizzer ls x = foldl foldFun (x, "") ls
+  where
+    iter :: Soda -> Int -> Text
+    iter (y, t) z
+      | mod z y == 0 = t
+      | otherwise    = ""
+
+    foldFun :: (Int, Text) -> Soda -> (Int, Text)
+    foldFun (y, b) a = (y, b <> iter a y)
